bb71faa250a84f7a4b0bf01293caff3d8c0a3483
yangg33 <yangrishaev9987@gmail.com>
2023-01-29 20:28:14 +0300

    Add obstacles

--- game/models.py
+++ game/models.py
@@ -1,11 +1,13 @@
 import pygame
 
+
 class Background(pygame.sprite.Sprite):
     def __init__(self, image_file, location):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(image_file)
         self.rect = self.image.get_rect()
         self.rect.left, self.rect.top = location
+
 
 class Bird(pygame.sprite.Sprite):
     def __init__(self, image_file, location):
@@ -15,17 +17,33 @@ class Bird(pygame.sprite.Sprite):
         self.location = location
         self.rect.centerx, self.rect.centery = self.location
 
-    def reduce_size (self, to):
-        self.image = pygame.transform.scale(self.image, (self.image.get_width() // to, self.image.get_height() //to))
+    def reduce_size(self, to):
+        self.image = pygame.transform.scale(self.image, (self.image.get_width() // to, self.image.get_height() // to))
         self.rect = self.image.get_rect()
         self.rect.centerx, self.rect.centery = self.location
+
 
 class FlyBird(Bird):
     def __init__(self, location):
         super().__init__("images/fly_bird.png", location)
-        self.reduce_size(8)
+        self.reduce_size(10)
+
 
 class DownBird(Bird):
     def __init__(self, location):
         super().__init__("images/down_bird.png", location)
-        self.reduce_size(8)
+        self.reduce_size(10)
+
+
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self, location):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("images/obstacle.png")
+        self.rect = self.image.get_rect()
+        self.location = location
+        self.rect.left, self.rect.top = location
+
+    def reduce_size(self, to):
+        self.image = pygame.transform.scale(self.image, (self.image.get_width() // to, self.image.get_height() // to))
+        self.rect = self.image.get_rect()
+        self.rect.centerx, self.rect.centery = self.location
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
+import random
+
 import pygame
 
-from game.models import Background, Bird, FlyBird, DownBird
+from game.models import Background, FlyBird, DownBird, Obstacle
 
 pygame.init()
 
 W = 1280
 H = 631
 FPS = 60
+speed = 2.5
 
 sc = pygame.display.set_mode((W, H))
 pygame.display.set_caption("Fly Bird")
@@ -16,31 +19,52 @@ clock = pygame.time.Clock()
 background = Background("images/background.jpg", [0, 0])
 sc.blit(background.image, background.rect)
 
-bird = FlyBird([W//2, H//2])
+bird = FlyBird([W // 2, H // 2])
 sc.blit(bird.image, bird.rect)
 
+obstacles = []
+
+
+def add_obstacle():
+    global obstacles
+    noise = random.randint(-100, 100)
+    obstacle1 = Obstacle([W + 30, H + noise])
+    obstacle1.reduce_size(1.6)
+
+    obstacle2 = Obstacle([W + 30, 0 + noise])
+    obstacle2.reduce_size(1.6)
+
+    return obstacle1, obstacle2
+
+
+obstacles.append(add_obstacle())
+
 ground = H - bird.image.get_height() // 2
-jump_force = 15
+jump_force = 8
 move = jump_force + 1
 gamestart = False
 is_jumped = False
 while True:
     for event in pygame.event.get():
-        if event.type== pygame.QUIT:
+        if event.type == pygame.QUIT:
             exit()
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE and not gamestart:
                 gamestart = True
-            elif event.key == pygame.K_SPACE and gamestart:
+                pygame.time.set_timer(pygame.USEREVENT, 2000)
+            if event.key == pygame.K_SPACE and gamestart:
+                bird = FlyBird([bird.rect.centerx, bird.rect.centery])
                 move = -jump_force
                 is_jumped = True
+        if event.type == pygame.USEREVENT:
+            obstacles.append(add_obstacle())
     if move <= 0:
         bird = DownBird([bird.rect.centerx, bird.rect.centery])
     else:
         bird = FlyBird([bird.rect.centerx, bird.rect.centery])
 
     if is_jumped:
-        if 0 <=  bird.rect.centery + move < ground:
+        if 0 <= bird.rect.centery + move < ground:
             bird.rect.centery += move
             move += 1
         elif bird.rect.centery + move < 0:
@@ -52,6 +76,17 @@ while True:
             is_jumped = False
 
     sc.blit(background.image, background.rect)
+
+    if gamestart:
+        for obst1, obst2 in obstacles:
+            obst1.rect.centerx -= speed
+            obst2.rect.centerx -= speed
+
+            sc.blit(obst1.image, obst1.rect)
+            sc.blit(obst2.image, obst2.rect)
+        while len(obstacles) != 0 and obstacles[0][0].rect.centerx < -30:
+            obstacles.pop(0)
+
     sc.blit(bird.image, bird.rect)
     pygame.display.update()
     clock.tick(FPS)
